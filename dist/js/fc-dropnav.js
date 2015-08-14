de4be9993d8d5fb3585f8d7cf8c1ff5d61f7ed28
Mark Cremer <mark@firstcoders.co.uk>
2015-08-14 01:58:46 -0700

    added code to prevent space from scrolling and removing handlers on scope destroy

--- dist/js/fc-dropnav.js
+++ dist/js/fc-dropnav.js
@@ -63,19 +63,40 @@ angular.module('fc-dropnav')
  */
 angular.module('fc-dropnav')
   .directive('fcDropnavToggle', function ($document, fcDropnavCtrl) {
-
     return {
       restrict: 'EA',
       scope: {
         keyCode: '=fcDropnavToggle'
       },
       link: function postLink($scope, element, attrs) {
-        $document.bind("keyup", function(event) {
-          if(event.which === $scope.keyCode &&
-            ['INPUT', 'TEXTAREA'].indexOf(angular.element(event.srcElement)[0].tagName) === -1
-          ) {
+        var isKeyCovered = function(e) {
+          return (e.which === $scope.keyCode &&
+            ['INPUT', 'TEXTAREA'].indexOf(angular.element(e.srcElement)[0].tagName) === -1
+          );
+        };
+
+        var handleKeyDown = function(e) {
+          //prevent space from scrolling down
+          if(isKeyCovered(e) && e.which === 32) {
+            e.preventDefault();
+            return false;
+          }
+        };
+
+        var handleKeyUp = function(e) {
+          if(isKeyCovered(e)) {
             fcDropnavCtrl.toggle();
           }
+        };
+
+        //attach event handlers
+        $document.bind("keydown", handleKeyDown);
+        $document.bind("keyup", handleKeyUp);
+
+        //on destroy remove handlers
+        $scope.$on('$destroy', function() {
+          $document.off(handleKeyDown);
+          $document.off(handleKeyUp);
         });
       }
     };
